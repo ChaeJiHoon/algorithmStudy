2a313599cd6bf8cb541150ecda94e1dbaa4c9759
Hoon <cjhking98@gmail.com>
2025-05-14 01:17:34 +0900

    [Silver IV] Title: 괄호, Time: 204 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++){
+            String str = sc.nextLine();
+            System.out.println(vps(str));
+        }
+
+    }
+
+    private static String vps(String str) {
+        String[] strArr = str.split("");
+        int count = 0;
+
+        for(int i=0;i<strArr.length;i++){
+            if(strArr[i].equals("(")){
+                count++;
+            }else count--;
+
+            if(count<0) return "NO";
+        }
+
+        if(count == 0) return "YES";
+        return "NO";
+    }
+}
