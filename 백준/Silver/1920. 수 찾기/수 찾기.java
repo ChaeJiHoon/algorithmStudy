a151fba3da63fba4734369c5cf6cfa443a7b4873
Hoon <cjhking98@gmail.com>
2025-05-14 21:58:19 +0900

    [Silver IV] Title: 수 찾기, Time: 1768 ms, Memory: 192692 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Set<Integer> set = new HashSet<>();
+
+        int M = sc.nextInt();
+
+        for(int i=0;i<M;i++){
+            set.add(sc.nextInt());
+        }
+
+        int N = sc.nextInt();
+
+        for(int i=0;i<N;i++){
+            int num = sc.nextInt();
+            if(set.contains(num)){
+                System.out.println(1);
+            }else System.out.println(0);
+        }
+    }
+}
