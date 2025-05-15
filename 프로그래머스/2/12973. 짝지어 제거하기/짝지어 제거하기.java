b1d9fefff99425ed5591a4651d81cf3b80fad42b
Hoon <cjhking98@gmail.com>
2025-05-15 17:11:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 67.88 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop(); // 짝이면 제거
+            } else {
+                stack.push(ch); // 아니면 추가
+            }
+        }
+            return stack.isEmpty() ? 1 : 0;
+    }
+}
