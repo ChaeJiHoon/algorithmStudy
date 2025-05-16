6eec5e3298cdce336f29692c8e9d38802309270f
Hoon <cjhking98@gmail.com>
2025-05-16 23:40:01 +0900

    프로그래머스, 백준

---
+++ src/practice/Practice2.java
@@ -0,0 +1,27 @@
+package practice;
+import java.io.*;
+
+public class Practice2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<t;i++){
+            String str = br.readLine();
+            int sum=0;
+            int count = 0;
+            for(int j=0;j<str.length();j++){
+                if(str.charAt(j) == 'O'){
+                    count++;
+                    sum += count;
+
+                }else{
+                    count = 0;
+                }
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
--- src/practice/practice.java
+++ src/practice/practice.java
@@ -1,22 +1,20 @@
 package practice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class practice {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int X = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        sc.nextLine();
-
-        String[] A = sc.nextLine().split(" ");
-
-        for(int i=0;i<N;i++){
-            if(X > Integer.valueOf(A[i])){
-                System.out.print(A[i] + " ");
-            }
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            String str = br.readLine();
+            sb.append(i+1).append("번째 받은 문자 내용 : ").append(str).append("\n");
         }
-
+        System.out.println(sb);
     }
 }
