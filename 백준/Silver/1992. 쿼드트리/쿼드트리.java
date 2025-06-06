0b6eafe0ff6aeadc1553db21c43449676a6004bf
Hoon <cjhking98@gmail.com>
2025-06-06 17:34:32 +0900

    [Silver I] Title: 쿼드트리, Time: 168 ms, Memory: 17180 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[][] map;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            String[] str = br.readLine().split("");
+            for(int j=0;j<n;j++){
+                map[i][j] = Integer.parseInt(str[j]);
+            }
+        }
+        System.out.println(quad(0,0,n));
+    }
+
+    static String quad(int row, int col, int size) {
+        if (isSame(row, col, size)) {
+            return Integer.toString(map[row][col]);
+        }
+
+        int half = size / 2;
+
+        return "("
+                + quad(row, col, half)                   // 왼쪽 위
+                + quad(row, col + half, half)            // 오른쪽 위
+                + quad(row + half, col, half)            // 왼쪽 아래
+                + quad(row + half, col + half, half)     // 오른쪽 아래
+                + ")";
+    }
+
+    static boolean isSame(int row, int col, int size) {
+        int val = map[row][col];
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (map[i][j] != val) return false;
+            }
+        }
+        return true;
+    }
+}
