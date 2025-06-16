1f8d6bcb8c423d5996d56da0a5c1a31fc4aaff13
Hoon <cjhking98@gmail.com>
2025-06-16 17:03:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 608 ms, Memory: 75960 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] tree;
+    static int[] parent;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        tree = new ArrayList[N+1];
+        parent = new int[N+1];
+        visited = new boolean[N+1];
+
+        // 인접 리스트 초기화
+        for(int i=1;i<=N;i++) tree[i] = new ArrayList<>();
+
+        // 간선 정보 입력
+        for(int i=0;i<N-1;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            tree[a].add(b);
+            tree[b].add(a);
+        }
+
+        dfs(1);
+
+        for(int i=2;i<=N;i++) sb.append(parent[i]).append("\n");
+
+        System.out.println(sb.toString().trim());
+
+    }
+
+    static void dfs(int now){
+        visited[now] = true;
+        for(int next : tree[now]){
+            if(!visited[next]){
+                parent[next] = now;// 현재 노드가 next의 부모
+                dfs(next);
+            }
+        }
+    }
+}
