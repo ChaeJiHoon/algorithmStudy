af86591f3de76b24624e2be1c67a1f7e2f7031c0
Hoon <cjhking98@gmail.com>
2025-09-10 22:30:55 +0900

    [Silver IV] Title: 로프, Time: 312 ms, Memory: 26288 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        for(int i=0;i<N;i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+
+        int max = 0;
+        for(int i=0;i<N;i++) max = Math.max(arr[i]*(N-i),max);
+
+        System.out.println(max);
+    }
+
+}
